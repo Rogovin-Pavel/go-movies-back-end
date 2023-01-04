901716aa2af04ea05158194d734e12fb3bf8ddee
p.rogovin <p.rogovin@devinotele.com>
2023-01-04 14:21:39 +0400

    [S9] GetUserByEmail is added

--- internal/repository/dbrepo/postgres_dbrepo.go
+++ internal/repository/dbrepo/postgres_dbrepo.go
@@ -64,3 +64,38 @@ func (m *PostgresDBRepo) AllMovies() ([]*models.Movie, error) {
 
 	return movies, nil
 }
+
+func (m *PostgresDBRepo) GetUserByEmail(email string) (*models.User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `
+		select
+			id, first_name, last_name, email,
+			password, created_at, updated_at
+		from users
+		where email = $1
+	`
+
+	row, err := m.DB.QueryContext(ctx, query, email)
+	if err != nil {
+		return nil, err
+	}
+	defer row.Close()
+
+	var user models.User
+	err = row.Scan(
+		&user.ID,
+		&user.FirstName,
+		&user.LastName,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
